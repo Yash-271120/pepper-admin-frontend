959d70b322663eee2a301e7d55c09996e0d1c7de
Yash Patil <yashpatil.yp2711@gmail.com>
2023-01-09 21:37:57 +0530

    Updated next-config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true
+},
+basePath:"/pepper-admin-frontend",
+assetPrefix:"/pepper-admin-frontend/",
 }
 
 module.exports = nextConfig
